2a4ea63c9dddfbc96e73d8fc1d3ace5482be6d7b
chervinsky92 <appdesigner2192@gmail.com>
2021-01-07 14:59:51 -0500

    Clean code and add comments

--- main.py
+++ main.py
@@ -6,41 +6,44 @@ from replit import clear
 print(logo)
 
 chosen_word = choice(word_list)
-# print(chosen_word)
+# print(f'Debugging: {chosen_word}')
+
 lives = 6
 end_of_game = False
 
+# display will hold remaining blanks and user's correct guesses
 display = []
-for _char in chosen_word:
+for char in chosen_word:
     display.append('_')
 
-while end_of_game == False:
+while not end_of_game:
     guess = input('Guess a letter: ').lower()
+    clear() # clear screen to avoid scrolling
 
-    clear()
-
+    # user has already guessed correct letter
     if guess in display:
         print(f' You have already guessed {guess}.')
 
+    # user makes correct guess -> display will update
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             display[i] = guess
-        
+
+    print(' '.join(display))
+
+    # user wins if display has no blanks remaining
     if '_' not in display:
         print('You win!')
         end_of_game = True
-        
-    print(' '.join(display))
-
+    
+    # user makes an incorrect guess -> loses a life
     if guess not in chosen_word:
         print(f"You guessed {guess} but that's not in the word. You lose a life.")
         lives -= 1
+        # user loses if lives reaches 0
         if lives == 0:
             print('You lose.')
             end_of_game = True
 
-    print(stages[lives])
-
-clear()
-    
-    
+    # display hangman image relative to user's remaining lives
+    print(stages[lives]) 
